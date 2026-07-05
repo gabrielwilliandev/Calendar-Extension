8bfe82371af4823258ece584d64cc3f61ce63d01
gabrielwilliandev <contatogwill@gmail.com>
2026-07-05 17:45:04 -0300

    Atualiza dependencias e configura ambiente local

--- public/js/app_chamadas.js
+++ public/js/app_chamadas.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://gnosis-api.whitesmoke-57ad5be1.eastus.azurecontainerapps.io/api';
+const API_BASE_URL = 'http://localhost:3000/api';
 let redirecionandoPorSessaoExpirada = false;
 
 function exibirAvisoSessaoExpirada(mensagem) {
